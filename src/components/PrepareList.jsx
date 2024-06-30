041f58a7aece0f998d7da6a28caaa4f733365048
Lamia Fatiha <f6lamia@gmail.com>
2024-06-30 23:38:51 +0600

    positioning done

--- src/components/PrepareList.jsx
+++ src/components/PrepareList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import chef from "../assets/images/chef.jpg";
+import chef from "../assets/images/chef2.png";
 
 const PrepareList = () => {
   return (
     <div className='md:mt-24 mt-8'>
        <h1 className='md:ml-16 md:mb-6 md:text-4xl ml-5 text-xl mb-4 font-bold'>You prepare the food, we handle the rest</h1>
-       <div>
+       <div className='relative'>
           <img src={chef} alt="chef" className='w-full h-1/2'  />
-          <div className='bg-gray-400'>
+          <div className='rounded-xl shadow-lg shadow-gray-600 w-1/2 absolute top-96 left-24 bg-white'>
              <h3>List your restaurant or shop on foodpanda</h3>
              <p>Would you like millions of new customers to enjoy your amazing food and groceries? So would we!
                 <br/>It&apos;s simple: we list your menu and product lists online, help you process orders, pick them up, and deliver them to hungry pandas – in a heartbeat!
