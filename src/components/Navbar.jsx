9c2b319630905b4d366f54afa0af87c7a28a1ac2
Lamia Fatiha <f6lamia@gmail.com>
2024-06-30 20:50:54 +0600

    content added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import logo from "../assets/images/logo.png";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import PersonIcon from '@mui/icons-material/Person';
+import LanguageIcon from '@mui/icons-material/Language';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Navbar = () => {
   return (
-    <h1>nav</h1>
+    <>
+        <div className='flex items-center justify-between'>
+          <div>
+            <img src={logo} alt="food" className=''/>
+            <span>QuickBite</span>
+          </div>
+          <div>
+             <div>
+                 <PersonIcon />
+                 <span>lamia</span>
+                 <KeyboardArrowDownIcon />
+             </div>
+             <div>
+                 <LanguageIcon />
+                 <span>lamia</span>
+                 <KeyboardArrowDownIcon />
+             </div>
+             <div>
+                 <ShoppingCartIcon />
+             </div>
+          </div>
+             
+        </div>
+    </>
   )
 }
 
