348963c342c31685b51bf73db29d5b38c9a2895d
Lamia Fatiha <f6lamia@gmail.com>
2024-06-30 23:41:30 +0600

    transparent bg

--- src/components/PrepareList.jsx
+++ src/components/PrepareList.jsx
@@ -7,7 +7,7 @@ const PrepareList = () => {
        <h1 className='md:ml-16 md:mb-6 md:text-4xl ml-5 text-xl mb-4 font-bold'>You prepare the food, we handle the rest</h1>
        <div className='relative'>
           <img src={chef} alt="chef" className='w-full h-1/2'  />
-          <div className='rounded-xl shadow-lg shadow-gray-600 w-1/2 absolute top-96 left-24 bg-white'>
+          <div className='rounded-xl shadow-lg shadow-gray-600 w-1/2 absolute top-96 left-24 bg-transparent text-white'>
              <h3>List your restaurant or shop on foodpanda</h3>
              <p>Would you like millions of new customers to enjoy your amazing food and groceries? So would we!
                 <br/>It&apos;s simple: we list your menu and product lists online, help you process orders, pick them up, and deliver them to hungry pandas – in a heartbeat!
