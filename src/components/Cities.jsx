ef66551220ea7a9cac726998bacf1b299ca7b318
Lamia Fatiha <f6lamia@gmail.com>
2024-07-01 12:15:27 +0600

    city section done

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -4,15 +4,17 @@ import {data} from "../constants/data"
 const Cities = () => {
   return (
     <>
-         <div className='mt-90 md:mt-80 '>
+         <div className='mt-96 pt-28 md:pt-0 md:mt-80 '>
          <h1 className='md:ml-16 md:mb-6 md:text-4xl ml-5 text-xl mb-4 font-bold'>
          Find us in these cities and many more!
          </h1>
-          <div className='grid md:grid-cols-4 gap-8'>
+          <div className='grid md:grid-cols-4 gap-8 px-16 py-4'>
              {data.map((text, index)=>(
-                <div key={index}>
-                   <img src={text.image} alt="city image" />
-                   <h3>{text.city}</h3>
+                <div key={index} className='relative cursor-pointer'>
+                   <img src={text.image} alt="city image" className='rounded-2xl h-52 w-full hover:scale-105
+                   transition delay-100' />
+                   <h3 className='py-2 px-4 bg-white rounded-lg absolute
+                   md:bottom-2 md:left-2 bottom-4 left-2 font-semibold md:text-md'>{text.city}</h3>
                 </div>
              ))}
              
