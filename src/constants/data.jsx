4477448b95df754cfd5c8b4c2de45c7592b94d26
Lamia Fatiha <f6lamia@gmail.com>
2024-07-01 11:54:19 +0600

    data file created

--- src/constants/data.jsx
+++ src/constants/data.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import dhaka from "../assets/images/dhaka.jpg";
+import barishal from "../assets/images/barishal.jpeg"
+import khulna from "../assets/images/khulna.jpeg"
+import syl from "../assets/images/syl.jpeg"
+import ctg from "../assets/images/ctg.jpg"
+import raj from "../assets/images/raj.jpg"
 
 
 
 export const Data = [
     {
-        image:
-        city:
-    }
+        image:dhaka,
+        city:"Dhaka",
+    },
+    {
+        image:barishal,
+        city:"Barishal",
+    }, 
+    {
+        image:khulna,
+        city:"Khulna",
+    }, 
+    {
+        image:syl,
+        city:"Sylhet",
+    }, 
+    {
+        image:ctg,
+        city:"Chittagong",
+    }, 
+    {
+        image:raj,
+        city:"Rajshahi",
+    },
 ]
