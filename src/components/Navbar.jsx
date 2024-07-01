b5591a8051d9d8834c831362a7962ddee3b9e10b
Lamia Fatiha <f6lamia@gmail.com>
2024-07-01 19:42:13 +0600

    navigation done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from "../assets/images/logo.png";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import PersonIcon from '@mui/icons-material/Person';
@@ -6,32 +7,37 @@ import LanguageIcon from '@mui/icons-material/Language';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
     <>
-        <div className='flex items-center justify-center md:justify-between  md:px-16 py-5 md:py-3 border-b-2 border-red-100 fixed w-full bg-white z-50'>
-          <div className='flex items-center justify-center'>
-            <img src={logo} alt="food" className='w-8 md:w-12 '/>
-            <span className='text-xl md:text-2xl mt-2 md:mt-3 ml-2 text-orange-500'>QuickBite</span>
-          </div>
-          <div className='flex items-center justify-around'>
-             <div className='px-4'>
-                 <PersonIcon />
-                 <span className='px-0 md:px-1 text-md text-gray-500'>Lamia</span>
-                 <KeyboardArrowDownIcon className='text-orange-500' />
-             </div>
-             <div className='px-4'>
-                 <LanguageIcon />
-                 <span className='px-0 md:px-1'>EN</span>
-                 <KeyboardArrowDownIcon className='text-orange-500' />
-             </div>
-             <div className='px-0 md:px-1 text-orange-500'>
-                 <ShoppingCartIcon />
-             </div>
-          </div>
-             
+      <div className='flex items-center justify-center md:justify-between md:px-16 py-5 md:py-3 border-b-2 border-red-100 fixed w-full bg-white z-50'>
+        <div className='flex items-center justify-center cursor-pointer' onClick={handleLogoClick}>
+          <img src={logo} alt="food" className='w-8 md:w-12' />
+          <span className='text-xl md:text-2xl mt-2 md:mt-3 ml-2 text-orange-500'>QuickBite</span>
         </div>
+        <div className='flex items-center justify-around'>
+          <div className='px-4'>
+            <PersonIcon />
+            <span className='px-0 md:px-1 text-md text-gray-500'>Lamia</span>
+            <KeyboardArrowDownIcon className='text-orange-500' />
+          </div>
+          <div className='px-4'>
+            <LanguageIcon />
+            <span className='px-0 md:px-1'>EN</span>
+            <KeyboardArrowDownIcon className='text-orange-500' />
+          </div>
+          <div className='px-0 md:px-1 text-orange-500'>
+            <ShoppingCartIcon />
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
